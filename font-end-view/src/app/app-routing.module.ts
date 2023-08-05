a50f25a034e502b71598f3d2413e7c702a610add
Nguyen Van Manh <113473329+ManhNV203@users.noreply.github.com>
2023-08-05 11:30:00 +0700

    create home view

--- font-end-view/src/app/app-routing.module.ts
+++ font-end-view/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './component/home/home.component';
+import {GiohangComponent} from './component/giohang/giohang.component';
+import {DetailsComponent} from './component/details/details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'shopping-cart', component: GiohangComponent},
+  { path: 'product-details', component: DetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
