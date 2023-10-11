ff463de2feaf5dd0d13f55e03fa91cd3791de4b2
nguyenxuan0411 <xuanntph21397@fpt.edu.vn>
2023-10-12 00:02:06 +0700

    update code giam gia

--- font-end-admin/src/app/app-routing.module.ts
+++ font-end-admin/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './component/home/home.component';
+import {ThemgiamgiaComponent} from "./component/themgiamgia/themgiamgia.component";
+import {ThemvoucherComponent} from "./component/themvoucher/themvoucher.component";
+import {BanggiamgiaComponent} from "./component/banggiamgia/banggiamgia.component";
+import {BangvoucherComponent} from "./component/bangvoucher/bangvoucher.component";
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'admin/dashboard', pathMatch: 'full'},
   {path: 'admin/dashboard', component: HomeComponent},
   {path: 'admin/don-hang', component: HomeComponent},
+  {path: 'admin/them-giam-gia', component: ThemgiamgiaComponent},
+  {path: 'admin/them-voucher', component: ThemvoucherComponent},
+  {path: 'admin/bang-giam-gia', component: BanggiamgiaComponent},
+  {path: 'admin/bang-voucher', component: BangvoucherComponent},
 ];
 
 @NgModule({
--- font-end-admin/src/app/app.module.ts
+++ font-end-admin/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AgGridModule} from "ag-grid-angular";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './layout/header/header.component';
 import {FooterComponent} from './layout/footer/footer.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { ThemgiamgiaComponent } from './component/themgiamgia/themgiamgia.component';
+import { ThemvoucherComponent } from './component/themvoucher/themvoucher.component';
+import { BanggiamgiaComponent } from './component/banggiamgia/banggiamgia.component';
+import { BangvoucherComponent } from './component/bangvoucher/bangvoucher.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +17,15 @@ import { SidebarComponent } from './layout/sidebar/sidebar.component';
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
+    ThemgiamgiaComponent,
+    ThemvoucherComponent,
+    BanggiamgiaComponent,
+    BangvoucherComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
