f20c38ed31293894b9bfaa9ad4017e444ff6b2cd
khaipro9xlc <110250192+khaipro9xlc@users.noreply.github.com>
2023-10-17 21:24:00 +0700

    Khai -- push code FE Product

--- font-end-admin/src/app/app.module.ts
+++ font-end-admin/src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {HeaderComponent} from './layout/header/header.component';
 import {FooterComponent} from './layout/footer/footer.component';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import {SidebarComponent} from './layout/sidebar/sidebar.component';
+import {DegiayComponent} from './component/degiay/degiay.component';
+import { ChatlieuComponent } from './component/chatlieu/chatlieu.component';
+import { MausacComponent } from './component/mausac/mausac.component';
+import { KichcoComponent } from './component/kichco/kichco.component';
+import { ThuonghieuComponent } from './component/thuonghieu/thuonghieu.component';
+import { ThemChatLieuComponent } from './component/chatlieu/them-chat-lieu/them-chat-lieu.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 @NgModule({
   declarations: [
@@ -13,12 +22,21 @@ import { SidebarComponent } from './layout/sidebar/sidebar.component';
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
+    DegiayComponent,
+    ChatlieuComponent,
+    MausacComponent,
+    KichcoComponent,
+    ThuonghieuComponent,
+    ThemChatLieuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ font-end-admin/src/app/component/chatlieu/them-chat-lieu/them-chat-lieu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-them-chat-lieu',
+  templateUrl: './them-chat-lieu.component.html',
+  styleUrls: ['./them-chat-lieu.component.css']
+})
+export class ThemChatLieuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
