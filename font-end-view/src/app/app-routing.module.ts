26e4b8647832de65c5d8d0624d3cbcb850effa45
Nguyen Van Manh <113473329+ManhNV203@users.noreply.github.com>
2023-10-26 21:33:13 +0700

    create view sign-in,sign-up

--- font-end-view/src/app/app-routing.module.ts
+++ font-end-view/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import {HomeComponent} from './component/home/home.component';
 import {GiohangComponent} from './component/giohang/giohang.component';
 import {DetailsComponent} from './component/details/details.component';
 import {SanphamComponent} from './component/sanpham/sanpham.component';
+import {LoginComponent} from './component/login/login.component';
+import {SignUpComponent} from './component/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -11,6 +13,8 @@ const routes: Routes = [
   { path: 'shopping-cart', component: GiohangComponent},
   { path: 'product-details', component: DetailsComponent},
   { path: 'sanpham', component: SanphamComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'sign-up', component: SignUpComponent}
 ];
 
 @NgModule({
